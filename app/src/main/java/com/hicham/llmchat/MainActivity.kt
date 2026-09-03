41ce28fdc37b87b1b5ae7908dfd0eae491983605
Loofy <ebh.ceo213@gmail.com>
2026-09-03 22:40:26 +0100

    refactor: compose viewmodel with model provider adapter

--- app/src/main/java/com/hicham/llmchat/MainActivity.kt
+++ app/src/main/java/com/hicham/llmchat/MainActivity.kt
@@ -3,20 +3,29 @@ package com.hicham.llmchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModelProvider
+import com.hicham.llmchat.data.AnthropicModelProvider
 import com.hicham.llmchat.ui.ChatScreen
 import com.hicham.llmchat.ui.ChatViewModel
 import com.hicham.llmchat.ui.LlmChatTheme
 import com.hicham.llmchat.ui.SettingsScreen
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: ChatViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : androidx.lifecycle.ViewModel> create(modelClass: Class<T>): T {
+                require(modelClass == ChatViewModel::class.java)
+                return ChatViewModel(application, AnthropicModelProvider(applicationContext)) as T
+            }
+        }
+        val viewModel = ViewModelProvider(this, factory)[ChatViewModel::class.java]
+
         setContent {
             LlmChatTheme {
                 Surface(modifier = Modifier) {
