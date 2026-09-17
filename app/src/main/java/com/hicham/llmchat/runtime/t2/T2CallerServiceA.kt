dcd882a22298e90c85380031f82d69bdd891e62c
Loofy <ebh.ceo213@gmail.com>
2026-09-17 13:54:36 +0100

    test(t2): isolate caller A durable store namespace

--- app/src/main/java/com/hicham/llmchat/runtime/t2/T2CallerServiceA.kt
+++ app/src/main/java/com/hicham/llmchat/runtime/t2/T2CallerServiceA.kt
@@ -1,3 +1,5 @@
 package com.hicham.llmchat.runtime.t2
 
-class T2CallerServiceA : T2CallerService()
+class T2CallerServiceA : T2CallerService() {
+    override val callerStoreNamespace: String = "callerA"
+}
